005dff46e0053efd790300e61c6f6d4eb51a7aa8
thientm27 <denkhotieu1@gmail.com>
2023-06-24 09:53:49 +0700

    demo signal R

---
+++ Study/SignalRLab/SignalRLab/Models/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SignalRLab.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+
+    }
+}
