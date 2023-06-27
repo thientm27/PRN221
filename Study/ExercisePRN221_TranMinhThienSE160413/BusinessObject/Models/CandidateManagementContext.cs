fe745bba70f15503d0a4657debed32f507268e55
thientm27 <denkhotieu1@gmail.com>
2023-06-27 09:47:19 +0700

    appsetting.json

--- Study/ExercisePRN221_TranMinhThienSE160413/BusinessObject/Models/CandidateManagementContext.cs
+++ Study/ExercisePRN221_TranMinhThienSE160413/BusinessObject/Models/CandidateManagementContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 #nullable disable
 
@@ -25,8 +27,18 @@ namespace BusinessObject.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =(local); database = CandidateManagement;uid=sa;pwd=123;TrustServerCertificate=True");
+                string connectString;
+                using (StreamReader r = new StreamReader("appsettings.json"))
+                {
+                    string json = r.ReadToEnd();
+                    IConfiguration config = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", true, true)
+                        .Build();
+                    connectString = config["ConnectionStrings:FUFlowerBouquetManagementDB"];
+                }
+
+                optionsBuilder.UseSqlServer(connectString);
             }
         }
 
